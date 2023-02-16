3569d2d6433a6f1d25be71bddcfbc559be7415ab
Bohdan Bortnyk <bohdan0211@gmail.com>
2023-02-16 18:49:07 +0200

    delete coments

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,34 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import {
-//   persistStore,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import { filterSlice } from './filter.slice';
-// import { persistContactsReducer } from './contact.slice';
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: persistContactsReducer,
-//     filter: filterSlice.reducer,
-//   },
-//   devTools: true,
-//   middleware(getDefaultMiddleware) {
-//     return getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     });
-//   },
-
-// });
-
-// export const persistor = persistStore(store);
-
 import { configureStore } from '@reduxjs/toolkit';
 import { contactApi } from './contactApi';
 
